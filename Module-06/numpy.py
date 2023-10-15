977cda082010a6ac91cb6553abc299dde09510b8
Sanketgadekar31 <gadekarsanket31700@gmail.com>
2023-10-15 14:49:29 +0530

    NumPy

---
+++ Module-06/numpy.py
@@ -0,0 +1,10 @@
+# Lerning NumPy | Python Learning - Module #06
+# NumPy is a Python library used for working with arrays.
+
+# It also has functions for working in domain of linear algebra, fourier transform, and matrices.
+
+# NumPy stands for Numerical Python.
+
+# https://numpy.org/
+
+# https://www.youtube.com/watch?v=Rbh1rieb3zc&list=PLu0W_9lII9ahKZ42vg2w9ERPmShYbYAB7&index=1&t=0s
